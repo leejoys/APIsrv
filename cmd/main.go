4b9d89f1fdb874f0cb037807ae784e4ba2d545e7
leejoys <44557202+leejoys@users.noreply.github.com>
2021-10-12 04:32:54 +0300

    mw ready

--- cmd/main.go
+++ cmd/main.go
@@ -36,5 +36,10 @@ func main() {
 	srv := server{}
 
 	srv.api = api.New()
+
+	// Запускаем веб-сервер на порту 8081 на всех интерфейсах.
+	// Предаём серверу маршрутизатор запросов.
+	log.Println("HTTP server is started on localhost:8080")
+	defer log.Println("HTTP server has been stopped")
 	log.Fatal(http.ListenAndServe("localhost:8080", srv.api.Router()))
 }
